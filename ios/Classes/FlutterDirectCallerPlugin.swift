9dfbcdfa7b1228d525a6826a2e8e3d0ae0d9e7db
Lester Rusike <41008271+Lesruez93@users.noreply.github.com>
2025-01-31 15:16:24 +0200

    added ios support

--- ios/Classes/FlutterDirectCallerPlugin.swift
+++ ios/Classes/FlutterDirectCallerPlugin.swift
@@ -10,10 +10,30 @@ public class FlutterDirectCallerPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
+    case "callNumber":
+      if let args = call.arguments as? [String: Any], let number = args["number"] as? String {
+        makeCall(to: number, result: result)
+      } else {
+        result(FlutterError(code: "INVALID_ARGUMENT", message: "Missing phone number", details: nil))
+      }
     default:
       result(FlutterMethodNotImplemented)
+    }
+  }
+
+  private func makeCall(to number: String, result: @escaping FlutterResult) {
+    #if targetEnvironment(simulator)
+    print("Running on iOS Simulator. Calls are not supported.")
+    result(FlutterError(code: "SIMULATOR_UNSUPPORTED", message: "Cannot make calls on a simulator", details: nil))
+    return
+    #endif
+
+    let formattedNumber = "tel://\(number)"
+    if let url = URL(string: formattedNumber), UIApplication.shared.canOpenURL(url) {
+      UIApplication.shared.open(url, options: [:], completionHandler: nil)
+      result(true)
+    } else {
+      result(FlutterError(code: "CALL_FAILED", message: "Cannot make a call", details: nil))
     }
   }
 }
